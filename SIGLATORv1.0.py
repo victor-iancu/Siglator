6459316488772f0175181d511811d5b1cba0d435
Victor <victor.iancu95@gmail.com>
2016-11-30 23:43:19 +0200

    - substantial improvements to the color recognition system

--- SIGLATORv1.0.py
+++ SIGLATORv1.0.py
@@ -48,7 +48,8 @@ class AppInterface(tk.Frame):
             if ext.lower() in validImages:
                 self.images.append(path.abspath(file))
 
-    def get_dominant_color(self, image):
+    '''
+        def get_dominant_color(self, image):
         width, height = image.size
         r_average = 0
         g_average = 0
@@ -79,13 +80,41 @@ class AppInterface(tk.Frame):
                     a_average = (a + a_average) / 2
 
             return (int(r_average), int(g_average), int(b_average), int(a_average))
+    '''
 
-    def verify_color(self, section_rgb, logo_rgb):
-        section_r, section_g, section_b = section_rgb
-        logo_r, logo_g, logo_b = logo_rgb
+    def verify_compatibility(self, section, accuracy=2):
+
+        width, height = section.size
+        width_step = int(width / accuracy)
+
+        print("Logo dominant color: ", self.logo_dominant_color)
+
+        for i in range(accuracy):
+
+            box = (width_step * i, 0, width_step * (i+1), height)
+            temp_section = section.crop(box)
+
+            bytesIO = BytesIO()
+            temp_section.save(bytesIO, 'JPEG')
+            bytesIO.seek(0)
+
+            ct = ColorThief(bytesIO)
+            try:
+                temp_section_dominant_color = ct.get_color(quality=5)
+            except:
+                print("Exception")
+                continue
+            print("Spot dominant color: ", temp_section_dominant_color)
+            if not self.verify_color(temp_section_dominant_color):
+                return False
+            bytesIO.close()
+        return True
+
+    def verify_color(self, section_dom_color_rgb):
+        section_r, section_g, section_b = section_dom_color_rgb
 
         for index in range(3):
-            if(abs(section_rgb[index] - logo_rgb[index]) >= 60):
+            if(abs(section_dom_color_rgb[index] - self.logo_dominant_color[index]) >= 60):
                 return True
 
         if abs(section_r - section_g) >= 30 and abs(section_r - section_b) >=30:
@@ -119,7 +148,6 @@ class AppInterface(tk.Frame):
             image_width_end_pixel = int(image_width * 3 / 100)
             image_height_end_pixel = int(image_height * 3 / 100)
 
-
             #size to scale the logo to
             max_logo_size = (image_width - image_width_begin_pixel - image_width_end_pixel, image_height - image_height_begin_pixel - image_height_end_pixel)
 
@@ -142,41 +170,16 @@ class AppInterface(tk.Frame):
                         image_height - image_height_end_pixel )
             section = im.crop(temp_box)
 
-            bytesIO = BytesIO()
-            section.save(bytesIO, 'JPEG')
-            bytesIO.seek(0)
-
-            #get image section's 4 most frequent colors
-            ct = ColorThief(bytesIO)
-            #spot_dominant_colors = ct.get_palette(color_count=2)
-            spot_dominant_color = ct.get_color(quality=5)
-            bytesIO.close()
-
-            #get logo's 4 most frequent colors
+            #get logo's dominant RGB
             ct2 = ColorThief(self.logoPath)
-            #logo_dominant_colors = ct2.get_palette(color_count=2)
-            logo_dominant_color = ct2.get_color(quality=5)
+            self.logo_dominant_color = ct2.get_color(quality=5)
 
-            print("Spot dominant color: ", spot_dominant_color)
-            #print("Spot secondary colors: ", spot_dominant_colors)
-            print("Logo dominant color: ", logo_dominant_color)
-            #print("Logo secondary colors: ", logo_dominant_colors)
-
-            if self.verify_color(spot_dominant_color, logo_dominant_color):
+            if self.verify_compatibility(section, 6):
                 print("Logo is good to be applied bottom-right!")
             else:
                 print("NOT a good position for the logo at bottom-right!")
 
-            #section.show()
-
-            #color_thief = ColorThief(self.images[0])
-            #print(color_thief.get_color(quality=1))
-            #print(color_thief.get_palette(color_count=10))
-
-
-
             #end of santier
-
 
             #load the logo in order to be able to paste it
             logo.load()
@@ -184,17 +187,7 @@ class AppInterface(tk.Frame):
             #paste the logo over the current image in the "box"
             im.paste(logo, box, mask=logo.split()[3])
 
-
             im.show()
-
-
-
-        '''for image in self.images:
-            print(image)
-            im = Image.open(image)
-            print(im.size, im.format)
-            width = im.size[0]
-            height = im.size[1]'''
 
 def main():
     root = tk.Tk()
@@ -202,19 +195,3 @@ def main():
     app.mainloop()
 
 main()
-
-
-''' code dump
-        #print("Logo width:",logo_width, "Logo height: ", logo_height)
-        #print("Future width: ", image_width - logo_width - i_width_out)
-        #print("future height: ", image_height - logo_height - i_height_out)
-        #box = (image_width - logo_width, image_height - logo_height, image_width, image_height)
-        #box = (image_width - logo_width - i_width_out, image_height - logo_height - i_height_out, i_width_out, i_height_out)
-
-        #print("Box width in: ", image_width - logo_width - i_width_out)
-        #print("Box height in: ", image_height - logo_height - i_height_out)
-        #print("Box width out: ", i_width_out)
-        #print("Box height out: ", i_height_out)
-        #print("Height size: ", image_height - (image_height - logo_height - i_height_out) - i_height_out)
-        #print("Width size: ", image_width - (image_width - logo_width - i_width_out) - i_width_out)
-'''
